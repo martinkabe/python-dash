b4049c606229f7dcbc23284e0b806726e7e788ec
Martin <martinkabe@seznam.cz>
2022-07-31 22:40:03 +0200

    Add layout-of-dashboards

--- Course-2/intro-to-plotly-dash/first_dashboard.py
+++ Course-2/intro-to-plotly-dash/first_dashboard.py
@@ -1,1 +1,15 @@
-import dash
+from distutils.log import debug
+from dash import Dash, html
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.Div(
+        id='my_div',
+        children='My Dashboard!'
+    )
+])
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ Course-2/layout-of-dashboards/html_components_overview.py
@@ -0,0 +1,5 @@
+from dash import Dash, html
+
+app = Dash(__name__)
+
+
